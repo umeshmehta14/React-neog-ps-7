264617c472bba6f722821dc480b736d64541d017
umeshmehta14 <ishaanmehta782@gmail.com>
2023-04-09 19:52:37 +0530

    Question 10 completed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Question4 from './Components/Question4';
 import Question5 from './Components/Question5';
 import Question6 from './Components/Question6';
 import Question7_8 from './Components/Question7_8';
+import Question9_10 from './Components/Question9_10';
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
       {/* <Question4/> */}
       {/* <Question5/> */}
       {/* <Question6/> */}
-      <Question7_8/>
+      {/* <Question7_8/> */}
+      <Question9_10/>
     </div>
   );
 }
